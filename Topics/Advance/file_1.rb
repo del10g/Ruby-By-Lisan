8a9eb48778600260c574f46810246a6e13ea1669
del10g <delledu35@gmail.com>
2023-04-17 22:38:31 +1000

    Added the module topics

--- Topics/Advance/file_1.rb
+++ Topics/Advance/file_1.rb
@@ -1,1 +1,22 @@
+require_relative 'file_2'
 
+include Tools
+
+class Main
+  attr_accessor :name, :age,:grade
+
+  def initialize(name,age,grade)
+    @name = name
+    @age = age
+    @grade = grade
+  end
+
+  def info
+    puts "My name is #{@name}. I am #{@age}. My grade is #{@grade}"
+    Tools.sayBye(@name)
+    Tools.sayHello(@name)
+  end
+end
+
+lisan = Main.new("Lisan Hosssain", 18 , 5.00 )
+lisan.info
---
+++ Topics/Advance/file_2.rb
@@ -0,0 +1,8 @@
+module  Tools
+  def sayHello(name)
+    puts "My name is #{name}, Hello from me"
+  end
+  def sayBye(name)
+    puts "My name is #{name}, Bye from me"
+  end
+end
